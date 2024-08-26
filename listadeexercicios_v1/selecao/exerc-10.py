f334b50f36f7925c7d358bae7ea1a95fdae7d6a5
Antonio Carlos da Silva Leitão <antoniocarlosdasilvaleitao4@gmail.com>
2024-08-26 11:10:30 -0300

    exercício 10 com lista

--- listadeexercicios_v1/selecao/exerc-10.py
+++ listadeexercicios_v1/selecao/exerc-10.py
@@ -7,7 +7,7 @@ valor2 = int(input("Digite o segundo valor: "))
 valor3 = int(input("Digite o terceiro valor: "))
 
 #processamento
-if ( valor1 < valor2 and valor2 < valor3 ):
+'''if ( valor1 < valor2 and valor2 < valor3 ):
     resposta = valor1 , " < ", valor2 , " < " , valor3
 elif ( valor1 < valor3 and valor3 < valor2):
     resposta = valor1 , " < ", valor3 , " < " , valor2
@@ -18,7 +18,19 @@ elif ( valor2 < valor1 and valor1 < valor3):
 elif ( valor1 < valor3 and valor3 < valor2):
     resposta = valor1 , " < ", valor1 , " < " , valor2
 else: #valor3 < valor2 and valor2 < valor3
-    resposta = valor3 , " < ", valor2 , " < " , valor3
+    resposta = valor3 , " < ", valor2 , " < " , valor3'''
+
+resposta = ""
+valorTemp = 0
+if( valor1 > valor2):
+    valorTemp = valor2
+    valor2 = valor1
+    valor1 = valorTemp
+
+if( valor1 > valor2):
+    valorTemp = valor2
+    valor2 = valor1
+    valor1 = valorTemp
 
 #saída
 print(str(resposta))
