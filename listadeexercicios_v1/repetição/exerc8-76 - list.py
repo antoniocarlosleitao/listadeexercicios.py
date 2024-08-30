bd786bc8e5f0700000a65126b4e8310bcbda4a95
Antonio Carlos da Silva Leitão <antoniocarlosdasilvaleitao4@gmail.com>
2024-08-30 18:29:45 -0300

    atualização exercícios

--- listadeexercicios_v1/repetição/exerc8-76 - list.py
+++ listadeexercicios_v1/repetição/exerc8-76 - list.py
@@ -6,10 +6,14 @@
 #OBS 2: Faça com que o sistema sorteie 6 valores digitados pelo usuário logo após exibir a mensagem. 
 #(random.range(1,51))
 
+
 maior = 0 
 menor = 999999999999
 soma =  0
 limite = 5
+
+listValores = [] #[] representa uma lista/array
+
 for x in range(1, limite+1): #for = repetições
     valor = int(input(f"Qual o {x}º valor?")) #x recebe os numeros e é o contador
     if(valor > maior):   #maior = ao maior valor digitado
@@ -18,8 +22,25 @@ for x in range(1, limite+1): #for = repetições
         menor = valor
     soma = soma + valor #a soma esta fora do if, mas dentro do for
     print("Soma = ", soma)
+    listValores.append(valor) #append acrescenta elementos a lista
 
 print("Média = ", int(soma/limite)) #se tirar o int a conta da média será float
 print("Maior número é: ", maior)
 print("Maior número é: ", menor)
+print("Valores: ", listValores)
+
+for itemValor in listValores:
+    if(itemValor % 2 == 0):
+        print("número par: ", itemValor)
+
+for itemValor in listValores:
+    if(itemValor % 2 != 0):
+        print("número impar =  ", itemValor)
+
+listPar = []
+listImpar = []
+
+for itemValor in listValores:
+    if(itemValor % 2 != 0):
+        print("número impar =  ", itemValor)
 
--- listadeexercicios_v1/repetição/exerc8-76 - radom.py
+++ listadeexercicios_v1/repetição/exerc8-76 - radom.py
@@ -6,20 +6,36 @@
 #OBS 2: Faça com que o sistema sorteie 6 valores digitados pelo usuário logo após exibir a mensagem. 
 #(random.range(1,51))
 
+import random
+
 maior = 0 
 menor = 999999999999
 soma =  0
 limite = 5
+listValores = [] #[] representa uma lista/array
+
 for x in range(1, limite+1): #for = repetições
-    valor = int(input(f"Qual o {x}º valor?")) #x recebe os numeros e é o contador
+    #valor = int(input(f"Qual o {x}º valor?")) #x recebe os numeros e é o contador
+    valor = random.randrange(1, 101)
     if(valor > maior):   #maior = ao maior valor digitado
         maior = valor   #valor = ao valor digitado
     if(valor < menor):   
         menor = valor
     soma = soma + valor #a soma esta fora do if, mas dentro do for
-    print("Soma = ", soma)
+    
+    listValores.append(valor) #append acrescenta elementos a lista
 
+print("Soma = ", soma)
 print("Média = ", int(soma/limite)) #se tirar o int a conta da média será float
 print("Maior número é: ", maior)
 print("Maior número é: ", menor)
+print("Valores: ", listValores)
+
+index = random.randrange(0,10)
+print("O número sorteado foi:(",index,") ", listValores[index])
+
+'''numeros = range(1, 61)
+resultado = random.sample(numeros, 6)
+print("Os números sorteados são:", resultado)'''
+
 
